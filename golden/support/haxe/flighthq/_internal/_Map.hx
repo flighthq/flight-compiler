90e188593e66991a0caba9206126c763cd3e405e
Joshua Granick <jgranick@users.noreply.github.com>
2026-09-06 17:27:03 -0700

    fix(golden): add get method to Haxe Map support type

--- golden/support/haxe/flighthq/_internal/_Map.hx
+++ golden/support/haxe/flighthq/_internal/_Map.hx
@@ -3,6 +3,7 @@ package flighthq._internal;
 class _Map<K, V> {
   public var size:Float;
   public function new() { size = 0; }
+  public function get(key:K):Null<V> { return null; }
   public function has(key:K):Bool { return false; }
   public function set(key:K, value:V):Void {}
   public function delete(key:K):Void {}
