55a6abed77c4674c3aa3cf3a67dc3f364d681818
Joshua Granick <jgranick@users.noreply.github.com>
2026-09-08 16:57:29 -0700

    feat(golden): add WeakMap Haxe support stub for compile check

---
+++ golden/support/haxe/flighthq/_internal/_WeakMap.hx
@@ -0,0 +1,9 @@
+package flighthq._internal;
+
+class _WeakMap<K, V> {
+  public function new() {}
+  public function get(key:K):Null<V> { return null; }
+  public function has(key:K):Bool { return false; }
+  public function set(key:K, value:V):Void {}
+  public function delete(key:K):Void {}
+}
