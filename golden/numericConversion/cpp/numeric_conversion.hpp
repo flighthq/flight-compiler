52436c57c1f9dd28ed882701298dc1213340430c
Joshua Granick <jgranick@users.noreply.github.com>
2026-09-08 20:45:09 -0700

    test(cpp): pin semantic exponentiation emission

--- golden/numericConversion/cpp/numeric_conversion.hpp
+++ golden/numericConversion/cpp/numeric_conversion.hpp
@@ -22,7 +22,7 @@ inline double lerp(double start, double end, double t) {
 }
 
 inline double round_to_places(double value, double places) {
-  const double factor = std::pow(10.0, places);
+  const double factor = flight::power(10.0, places);
   return (flight::round((value * factor)) / factor);
 }
 
