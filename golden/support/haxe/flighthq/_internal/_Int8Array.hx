af661af1d86b86bc7c32c8b59a68bc6d1459a88f
Joshua Granick <jgranick@users.noreply.github.com>
2026-09-09 06:29:45 -0700

    fix(golden): add typed array Haxe support stubs for compile-check

--- golden/support/haxe/flighthq/_internal/_Int8Array.hx
+++ golden/support/haxe/flighthq/_internal/_Int8Array.hx
@@ -1,9 +1,9 @@
 package flighthq._internal;
 
-abstract _UInt8Array(Array<Float>) {
+abstract _Int8Array(Array<Float>) {
   public inline function new(arg:Dynamic = null) { this = []; }
   @:arrayAccess public inline function get(index:Float):Float { return this[Std.int(index)]; }
   @:arrayAccess public inline function set(index:Float, value:Float):Float { this[Std.int(index)] = value; return value; }
-  public inline function subarray(begin:Float = 0, end:Float = 0):_UInt8Array { return new _UInt8Array(); }
-  public inline function slice(begin:Float = 0, end:Float = 0):_UInt8Array { return new _UInt8Array(); }
+  public inline function subarray(begin:Float = 0, end:Float = 0):_Int8Array { return new _Int8Array(); }
+  public inline function slice(begin:Float = 0, end:Float = 0):_Int8Array { return new _Int8Array(); }
 }
--- golden/support/haxe/flighthq/_internal/_UInt8ClampedArray.hx
+++ golden/support/haxe/flighthq/_internal/_UInt8ClampedArray.hx
@@ -1,9 +1,9 @@
 package flighthq._internal;
 
-abstract _UInt8Array(Array<Float>) {
+abstract _UInt8ClampedArray(Array<Float>) {
   public inline function new(arg:Dynamic = null) { this = []; }
   @:arrayAccess public inline function get(index:Float):Float { return this[Std.int(index)]; }
   @:arrayAccess public inline function set(index:Float, value:Float):Float { this[Std.int(index)] = value; return value; }
-  public inline function subarray(begin:Float = 0, end:Float = 0):_UInt8Array { return new _UInt8Array(); }
-  public inline function slice(begin:Float = 0, end:Float = 0):_UInt8Array { return new _UInt8Array(); }
+  public inline function subarray(begin:Float = 0, end:Float = 0):_UInt8ClampedArray { return new _UInt8ClampedArray(); }
+  public inline function slice(begin:Float = 0, end:Float = 0):_UInt8ClampedArray { return new _UInt8ClampedArray(); }
 }
